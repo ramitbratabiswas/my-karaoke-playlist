44a3175f785b4038d59a8a5c0a0fa69cfacd7e45
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-06-16 18:20:58 -0700

    added lyrics to data

--- react-interview-prep/src/components/accordion/data.js
+++ react-interview-prep/src/components/accordion/data.js
@@ -2,26 +2,263 @@ export const data = [
   {
     id: 1,
     song: 'Play No Games',
-    artist: ['Henry Young', 'Ashley Alisha']
+    artist: ['Henry Young', 'Ashley Alisha'],
+    lyrics: `Hey babe all I want is to be loved by you
+But you would never take me as I am it's true
+소중한 우리 사이가 멀어질까봐
+멀어질까봐
+I don't know
+Oh 마음처럼
+뭐라고 하면 날 떠나버릴까봐 no
+나를 밀다 땡겼다 하는 너
+그렇게라도 나랑 있는게 난 좋아 no
+참는데도 한계가 있어
+이제는 더 이상 못참겠다는 말야 yeah
+그니까 날 좀 똑바로 봐봐
+진심으로 너만 바라보잖아
+싫음 싫다고 얼른 말해 바보야
+계속 기다리게 하지마 Goodbye
+생각보다 내가 너무 여린 가봐
+이런 게임은 내가 무조건 져 난
+몰라 몰라, 사랑이란
+모르겠으니까, 장난 좀 치지마
+알면서도 모르는척은 또 뭐야
+울고 웃게하는 너도 참 나빠
+몰라 몰라, 사랑이란
+모르겠으니까, 장난 좀 치지마
+장난 좀 치지마
+하지마 하지마
+장난 좀 치지마
+Over this game of love
+알면서도 모르는척은 또 뭐야
+울고 웃게하는 너도 참 나빠
+몰라 몰라, 사랑이란
+모르겠으니까, 장난 좀 치지마
+밀다 땡겼다 하는 너
+그렇게라도 나랑 있는게 난 좋아 no
+참는데도 한계가 있어
+이제는 더 이상 못참겠다는 말야 yeah
+그니까 날 좀 똑바로 봐봐
+진심으로 너만 바라보잖아
+싫음 싫다고 얼른 말해 바보야
+계속 기다리게 하지마 Goodbye
+생각보다 내가 너무 여린 가봐
+이런 게임은 내가 무조건 져 난
+몰라 몰라, 사랑이란
+모르겠으니까, 장난 좀 치지마
+알면서도 모르는척은 또 뭐야
+울고 웃게하는 너도 참 나빠
+몰라 몰라, 사랑이란
+모르겠으니까, 장난 좀 치지마
+장난 좀 치지마
+하지마 하지마
+장난 좀 치지마
+Over this game of love
+알면서도 모르는척은 또 뭐야
+울고 웃게하는 너도 참 나빠
+몰라 몰라, 사랑이란
+모르겠으니까, 장난 좀 치지마
+Hey babe all I want is to be loved by you
+But you would never take me as I am it's true`
   },
   {
     id: 2,
     song: 'Turn It Up',
-    artist: ['PinkPantheress']
+    artist: ['PinkPantheress'],
+    lyrics: `Yeah, yeah, yeah, yeah, yeah
+Yeah, yeah, yeah, yeah, yeah
+Yeah, yeah, yeah, yeah, yeah
+Every time I think of you
+The DJ hit me right there (right there)
+Said he loved me right there (right there)
+Shouted, "Shake it" (shake it)
+I ninety-nine percent can't move my waistline
+Come sit with me, right there (right there)
+Maybe kiss me, right there (right there)
+I'm not faking
+A good time with you, that's what I want tonight
+Why, just show your devotion
+Let yourself to the motions
+I hate going out, but you just got the promotion
+You go to this club
+You sing along to look social
+To your favorite song
+You just make me wanna say, "Hey, it's me" (hey, it's me)
+We've been talking twice a week (we've been talking twice a week)
+I like this beat (I like this beat)
+It just makes me wanna sing (it just makes me wanna sing, turn it up)
+"Hey, it's me" (hey, it's me)
+We've been talking twice a week (we've been talking twice a week)
+I like this beat (I like this beat)
+It just makes me wanna sing (it just makes me wanna sing)
+Oh, Maria (yeah, yeah, yeah, yeah, yeah)
+Maria (yeah, yeah, yeah, yeah, yeah)
+Oh, Maria (every time I think of you)
+I won't tell them I met you in the club (club, club, club)
+You remember our favorite song was playing
+And every word that came out your mouth was wrong
+Oh, Maria, Maria
+Tell me why you're always here at night? (Turn it up)
+It seems to me it's the only time I see ya
+And when I thought I found purpose in my life
+You're not here
+You just make me wanna say, "Hey, it's me" (hey, it's me)
+We've been talking twice a week (we've been talking twice a week)
+I like this beat (I like this beat)
+It just makes me wanna sing (it just makes me wanna sing, turn it up)
+"Hey, it's me" (hey, it's me)
+We've been talking twice a week (we've been talking twice a week)
+I like this beat (I like this beat)
+It just makes me wanna sing (it just makes me wanna sing)
+Maria (yeah, yeah, yeah, yeah, yeah)
+Oh, Maria (yeah, yeah, yeah, yeah, yeah)
+Maria (yeah, yeah, yeah, yeah, yeah)
+Every time I think of you, Maria
+(Oh Maria, Maria)`
   },
   {
     id: 3,
-    song: 'I wanna tell you',
-    artist: ['Lexie Liu']
+    song: 'i wanna tell u',
+    artist: ['Lexie Liu'],
+    lyrics: `花 褪去花瓣
+梦随梦消散在梦里
+哼 我自己的歌
+写给你的音符藏不住
+躺在你身边
+怎么睡得着
+牵你不松手
+Okay, okay, okay
+原来爱可以很简单
+We can float and listen
+Okay, okay, okay
+不需要找什么答案
+Let's just flow endlessly
+I wanna love you 'cause I just can't help to
+I wanna kiss you but is that gon' be an issue?
+'Cause you have heard enough of this
+'Cause you have heard enough of this
+I wanna tell you how much I do miss you
+I wanna tell you everything but I do miss you
+'Cause you have heard enough of this
+'Cause you have heard enough of this
+I know, I know, I know you'vе heard it
+I know you've heard a million timеs now
+Love, is it a crime?
+Killing my pride so tenderly
+哼 没节奏的歌
+呼吸偷走心跳的舞步
+躺在你身边
+怎么睡得着
+牵你不松手
+Okay, okay, okay
+原来爱可以很简单
+We can float and listen
+Okay, okay, okay
+不需要找什么答案
+Let's just flow endlessly
+I wanna love you 'cause I just can't help to
+I wanna kiss you but is that gon' be an issue?
+'Cause you have heard enough of this
+'Cause you have heard enough of this
+I wanna tell you how much I do miss you
+I wanna tell you everything but I do miss you
+'Cause you have heard enough of this
+'Cause you have heard enough of this`
   },
   {
     id: 4,
     song: 'Please Please Please',
-    artist: ['Sabrina Carpenter']
+    artist: ['Sabrina Carpenter'],
+    lyrics: `I know I have good judgment, I know I have good taste
+It's funny and it's ironic that only I feel that way
+I promise 'em that you're different and everyone makes mistakes
+But just don't
+I heard that you're an actor, so act like a stand-up guy
+Whatever devil's inside you, don't let him out tonight
+I tell them it's just your culture and everyone rolls their eyes
+Yeah, I know
+All I'm asking, baby
+
+Please, please, please
+Don't prove I'm right
+And please, please, please
+Don't bring me to tears when I just did my makeup so nice
+Heartbreak is one thing, my ego's another
+I beg you, don't embarrass me, motherfucker, oh
+Please, please, please (Ah)
+
+Well, I have a fun idea, babe (Uh-huh), maybe just stay inside
+I know you're cravin' some fresh air, but the ceiling fan is so nice (It's so nice, right?)
+And we could live so happily if no one knows that you're with me
+I'm just kidding, but really (Kinda), really, really
+
+Please, please, please (Please don't prove I'm right)
+Don't prove I'm right
+And please, please, please
+Don't bring me to tears when I just did my makeup so nice
+Heartbreak is one thing (Heartbreak is one thing), my ego's another (Ego's another)
+I beg you, don't embarrass me, motherfucker, oh
+Please, please, please (Ah)
+
+If you wanna go and be stupid
+Don't do it in front of me
+If you don't wanna cry to my music
+Don't make me hate you prolifically
+Please, please, please (Please)
+Please, please, please (Please)
+Please (Please), please (Please), please
+(Ah)`
   },
   {
     id: 5,
     song: 'Good Luck, Babe!',
-    artist: ['Chappell Roan']
+    artist: ['Chappell Roan'],
+    lyrics: `It's fine, it's cool
+You can say that we are nothing, but you know the truth
+And guess I'm the fool
+With her arms out like an angel through the car sunroof
+I don't wanna call it off
+But you don't wanna call it love
+You only wanna be the one that I call "baby"
+You can kiss a hundred boys in bars
+Shoot another shot, try to stop the feeling
+You can say it's just the way you are
+Make a new excuse, another stupid reason
+Good luck, babe (well, good luck), well, good luck, babe (well, good luck)
+You'd have to stop the world just to stop the feeling
+Good luck, babe (well, good luck), well, good luck, babe (well, good luck)
+You'd have to stop the world just to stop the feeling
+I'm cliché, who cares?
+It's a sexually explicit kind of love affair
+And I cry, it's not fair
+I just need a little lovin', I just need a little air
+Think I'm gonna call it off
+Even if you call it love
+I just wanna love someone who calls me "baby"
+You can kiss a hundred boys in bars
+Shoot another shot, try to stop the feeling
+You can say it's just the way you are
+Make a new excuse, another stupid reason
+Good luck, babe (well, good luck), well, good luck, babe (well, good luck)
+You'd have to stop the world just to stop the feeling
+Good luck, babe (well, good luck), well, good luck, babe (well, good luck)
+You'd have to stop the world just to stop the feeling
+When you wake up next to him in the middle of the night
+With your head in your hands, you're nothing more than his wife
+And when you think about me, all of those years ago
+You're standing face to face with "I told you so"
+You know I hate to say, "I told you so"
+You know I hate to say, but, I told you so
+You can kiss a hundred boys in bars
+Shoot another shot, try to stop the feeling (well, I told you so)
+You can say it's just the way you are
+Make a new excuse, another stupid reason
+Good luck, babe (well, good luck), well, good luck, babe (well, good luck)
+You'd have to stop the world just to stop the feeling
+Good luck, babe (well, good luck), well, good luck, babe (well, good luck)
+You'd have to stop the world just to stop the feeling
+You'd have to stop the world just to stop the feeling
+You'd have to stop the world just to stop the feeling
+You'd have to stop the world just to stop the feeling`
   }
 ]
