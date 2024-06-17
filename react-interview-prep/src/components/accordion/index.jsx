e9a5ff306dccf15d5223fb7aebfc33783486634f
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-06-16 22:14:34 -0700

    added and commented genius API import

--- react-interview-prep/src/components/accordion/index.jsx
+++ react-interview-prep/src/components/accordion/index.jsx
@@ -4,6 +4,8 @@
 import { useState } from "react";
 import { data } from './data.js';
 
+import { getLyrics, getSong } from 'genius-lyrics-api';
+
 export default function Accordion() {
 
   const [selected, setSelected] = useState(null);
@@ -29,6 +31,15 @@ export default function Accordion() {
     setMultipleSelected(newSelected);
     console.log(newSelected);
   }
+
+  const options = {
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXX',
+    title: 'Posthumous Forgiveness',
+    artist: 'Tame Impala',
+    optimizeQuery: true
+  };
+  
+  getLyrics(options).then((lyrics) => console.log(lyrics));
 
   return (
     <div className="wrapper">
--- react-interview-prep/src/main.jsx
+++ react-interview-prep/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import Accordion from './components/accordion/index.jsx'
 import './components/accordion/styles.css';
 
+// import { getLyrics, getSong } from 'genius-lyrics-api';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Accordion/>
