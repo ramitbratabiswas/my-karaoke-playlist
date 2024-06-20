992dee884f2878b698141ce10c860eedec64bf67
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-06-20 15:06:15 -0700

    changed base address on vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/spotify-top-songs-and-lyrics/"
+  base: "/my-karaoke-playlist/"
 })
