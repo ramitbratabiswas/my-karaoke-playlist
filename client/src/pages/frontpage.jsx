8ee639536285cacd8d670c4c0f3c7ea22ae7b829
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-06-28 00:22:07 -0700

    added links to homepage

--- client/src/pages/frontpage.jsx
+++ client/src/pages/frontpage.jsx
@@ -1,9 +1,13 @@
-import Navbar from "../components/navbar";
+import { Link } from "react-router-dom";
 
 export default function Frontpage() {
   return (
     <div>
       <h1>Home</h1>
+      <ul>
+        <h1><li><Link to="/userplaylists">My playlists</Link></li></h1>
+        <h1><li><Link to="/spotifyplaylists">Spotify top playlists</Link></li></h1>
+      </ul>
     </div>
   );
 }
