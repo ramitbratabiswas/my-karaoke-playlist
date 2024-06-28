8ff41fb07e6d848efd6d3cda00ddbe49b2e1eada
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-06-27 21:57:31 -0700

    added basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+
 import Accordion from './components/accordion.jsx'
 import Navbar from './components/navbar.jsx'
-import SpotifyPlaylists from './components/spotifyplaylists.jsx';
+import SpotifyPlaylists from './components/spotifyPlaylists.jsx';
 import UserPlaylists from './components/userPlaylists.jsx';
 import { playlists } from './data/playlists.js';
+
+import './assets/styles/main.css'
 
 export const App = () => {
   return (
     <>
-      <div className='navbar'>
-        <Navbar />
-      </div>
-      <UserPlaylists />
-      <SpotifyPlaylists />
+      <Navbar />
+      <Routes>
+        <Route path="/1" element={<SpotifyPlaylists />} />
+        <Route path="/2" element={<UserPlaylists />} />
+        <Route path="/3" element={<Accordion playlistId={playlists[0].id} />} />
+      </Routes>
     </>
   );
 }
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (<>
     <div className='header'>
-      <h1>my <span id='karaoke'>karaoke</span> playlist!</h1>
+      <Link to="/"><h1>my <span id='karaoke'>karaoke</span> playlist!</h1></Link>
     </div>
     <div className='profile icon'>
     </div>
