7280d12e3691b2312d0eaa4eae49e0a2d13ffbe4
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2025-05-06 14:10:46 -0700

    added back button functionality and styling

--- src/components/accordion.jsx
+++ src/components/accordion.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useFetchLyrics } from "../utils/fetchLyrics.js"
 import { useScrapeLyrics } from "../utils/scrapeLyrics.js";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,12 +8,12 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 export default function Accordion({ data }) {
 
   const { name, tracks } = data;
-
   // selected denotes the index of the selected track
   const [selected, setSelected] = useState(-1);
-
   let [lyrics, loading] = useScrapeLyrics(selected > 0 ? tracks[selected-1].song : "", selected > 0 ? tracks[selected-1].artist : []);
   // let [lyrics, loading] = useFetchLyrics(selected > 0 ? tracks[selected-1].song : "", selected > 0 ? tracks[selected-1].artist : []);
+
+  const navigate = useNavigate();
 
   function handleSingleSelection(currentId) {
     if (selected !== currentId) {
@@ -24,10 +25,12 @@ export default function Accordion({ data }) {
 
   return (
     <div>
-      <div className="list-heading">
-        <h1 id="accordion-heading-back"><FontAwesomeIcon icon={faArrowLeft}/></h1>
+      {/* delay name load */}
+      { name ? <div className="list-heading">
+        <h1 id="accordion-heading-back" onClick={() => navigate(-1)}><FontAwesomeIcon icon={faArrowLeft}/></h1>
         <h1 id="accordion-heading-name">{name}</h1>
-      </div>
+      </div> : null}
+      
       <div className="wrapper accordion">
         {
           tracks && tracks.length > 0 ?
